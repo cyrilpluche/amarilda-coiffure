02f67d56f2fdc6142b0df9aa86d6c7a94c22114e
Pluchezerrr <cyril.pluche@etu.umontpellier.fr>
2018-12-25 19:29:35 +0100

    fix 4

--- server/app.js
+++ server/app.js
@@ -31,9 +31,9 @@ app.use('/api', controllers.EndPoint.send);
 // Use React App
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, '../client/build')));
+    app.use(express.static(path.resolve(__dirname, './client/build')));
     app.get('*', function(request, response) {
-        response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+        response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
     });
 }
 
